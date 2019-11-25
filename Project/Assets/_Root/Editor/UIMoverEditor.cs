f7626d9b14a891580878d428471f05a28d6e9795
Sekkmer <sekkmer@gmail.com>
2019-11-25 16:51:14 +0100

    more UI control

---
+++ Project/Assets/_Root/Editor/UIMoverEditor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(UIMover))]
+public class UIMoverEditor : Editor
+{
+    UIMover Target => target as UIMover;
+
+    bool _allowMove = true;
+    bool _allowResize = true;
+    bool _allowClose = true;
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Validate")) {
+            Target.Validate();
+        }
+        if (_allowMove != Target.allowMove) {
+            _allowMove = !_allowMove;
+            Target.moveButton.gameObject.SetActive(_allowMove);
+        }
+        if (_allowResize != Target.allowResize) {
+            _allowResize = !_allowResize;
+            Target.resizeButton.gameObject.SetActive(_allowResize);
+        }
+        if (_allowClose != Target.allowClose) {
+            _allowClose = !_allowClose;
+            Target.closeButton.gameObject.SetActive(_allowClose);
+        }
+    }
+}
+
--- Project/Assets/_Root/Scripts/UI/Util/UIMover.cs
+++ Project/Assets/_Root/Scripts/UI/Util/UIMover.cs
@@ -1,39 +1,57 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem.UI;
+using UnityEngine.UI;
 
 public class UIMover : MonoBehaviour
 {
     public RectTransform canvas;
     public RectTransform parent;
     public HoldButton moveButton;
+    public HoldButton resizeButton;
+    public Button closeButton;
 
-    public bool validate;
+    public bool allowMove = true;
+    public bool allowResize = true;
+    public bool allowClose = true;
 
     // public bool
 
     private bool _move = false;
+    private bool _resize = false;
+
     private Vector2 _point;
     private Vector2 _pointLast;
 
-    private void Awake()
-    {
-        moveButton.onDown.AddListener(() => { _move = true; _pointLast = _point; });
-        moveButton.onUp.AddListener(() => _move = false);
+    private void Awake() {
+        if (allowMove) {
+            moveButton.gameObject.SetActive(allowMove);
+
+            moveButton.onDown.AddListener(() => { _move = true; _pointLast = _point; });
+            moveButton.onUp.AddListener(() => _move = false);
+        }
+
+        if (allowResize) {
+            resizeButton.gameObject.SetActive(allowMove);
+
+            resizeButton.onDown.AddListener(() => { _resize = true; _pointLast = _point; });
+            resizeButton.onUp.AddListener(() => _resize = false);
+        }
+
+        if (allowClose) {
+            closeButton.onClick.AddListener(() => parent.gameObject.SetActive(false));
+        }
 
         FindObjectOfType<InputSystemUIInputModule>().point.action.performed += ctx => _point = ctx.ReadValue<Vector2>();
     }
 
-    private void OnValidate()
-    {
+    public void Validate() {
         parent.pivot = Vector2.zero;
-        parent.anchorMin = new Vector2(0.5f, 0.5f);
-        parent.anchorMax = new Vector2(0.5f, 0.5f);
+        parent.anchorMin = new Vector2(0.0f, 0.0f);
+        parent.anchorMax = new Vector2(0.0f, 0.0f);
     }
 
-    private void Update()
-    {
-        if (_move)
-        {
+    private void Update() {
+        if (_move && allowMove) {
             Vector3 delta = _pointLast - _point;
             _pointLast = _point;
 
@@ -42,6 +60,10 @@ public class UIMover : MonoBehaviour
             parent.position = new Vector3(
                 Mathf.Clamp(parent.position.x, 0, bounds.x),
                 Mathf.Clamp(parent.position.y, 0, bounds.y));
+        }
+        if (_resize && allowResize) {
+            parent.sizeDelta -= _pointLast - _point;
+            _pointLast = _point;
         }
     }
 }
